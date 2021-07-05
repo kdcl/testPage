22d7dc5ccfc9041d5b0f13d86cf282e927634d25
kdcl <kdcl03@gmail.com>
2021-07-05 20:29:18 +0800

    add dist

---
+++ dist/js/about.dd332ba4.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(n,t,c){"use strict";c.r(t);var e=c("7a23"),a={class:"about"},s=Object(e["g"])("h1",null,"This is an my kdcl page",-1);function o(n,t){return Object(e["p"])(),Object(e["d"])("div",a,[s])}const u={};u.render=o;t["default"]=u}}]);
+//# sourceMappingURL=about.dd332ba4.js.map
